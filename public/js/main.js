4dc06425df2e68193716a327659378c406244808
Ramsés <ramses4323@gmail.com.br>
2021-10-28 23:02:55 -0300

    testes db

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,24 @@
-const firebase = require("firebase");
+const firebaseConfig = {
+    apiKey: "AIzaSyAqDmRh4jLFRNBPk66HXP0jJQMdwWXrJvI",
+    authDomain: "sesinovoensinomedio.firebaseapp.com",
+    projectId: "sesinovoensinomedio",
+    appId: "1:476150079392:web:a4bbe058602ec6ee308ec5",
+  };
 
-firebase.initializeApp({
-    apiKey: "",
-    authDomain:"",
-    projectId:"",
-})
+start()
+function start(){
+    const firebase = require("firebase-admin");
+
+firebase.initializeApp(firebaseConfig)
 var db = firebase.firestore();
+
+console.log("teste")
+
+db.collection("conteúdos").doc("teste").get().then(function(doc){
+    if(doc.exists){
+        console.log("existe")
+    }else{
+        console.log("Não existe")
+    }
+})
+}
